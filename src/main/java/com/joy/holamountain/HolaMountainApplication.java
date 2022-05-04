783f33e45fba3b73e64416fc41df1572e6cf6092
diqksrk <diqksrk123@naver.com>
2022-05-04 21:56:42 +0900

    docs : Init setting

---
+++ src/main/java/com/joy/holamountain/HolaMountainApplication.java
@@ -0,0 +1,13 @@
+package com.joy.holamountain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMountainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolaMountainApplication.class, args);
+    }
+
+}
